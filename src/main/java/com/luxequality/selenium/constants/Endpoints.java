0a7aeddfdc6bf8018d791d06388dcb537ee30277
AndriiCar <kravchenko.svg@gmail.com>
2023-12-10 13:26:44 +0200

    created constants for endpoints and test users

---
+++ src/main/java/com/luxequality/selenium/constants/Endpoints.java
@@ -0,0 +1,5 @@
+package com.luxequality.selenium.constants;
+
+public class Endpoints {
+    public static final String inventoryPage = "inventory.html";
+}
---
+++ src/main/java/com/luxequality/selenium/constants/TestUsers.java
@@ -0,0 +1,6 @@
+package com.luxequality.selenium.constants;
+
+public class TestUsers {
+    public static final String standardUser = "standard_user";
+    public static final String password = "secret_sauce";
+}
